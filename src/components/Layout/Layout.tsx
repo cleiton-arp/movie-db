6ad0b33b19293cb22479623ccb2b2a1af45b27f2
Cleiton <cleiarap@gmail.com>
2025-10-30 22:32:08 -0300

    chore: retirando padding para exibição mobile

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,12 +2,10 @@ import styled from "styled-components";
 import { Header } from "../Header";
 
 const HEADER_HEIGHT = 50;
-const HEADER_HEIGHT_MOBILE = 200;
 
 const ContentWrapper = styled.div`
   padding-top: ${HEADER_HEIGHT}px; /* espaço reservado para o header fixo */
   @media (max-width: 800px) {
-    padding-top: ${HEADER_HEIGHT_MOBILE}px;
   }
 `;
 
